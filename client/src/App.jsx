09c70944131df7f48b39779970c2bb52c6d8ad0b
jorgelerma <jorge@jorgelerma.com>
2018-04-15 22:20:33 -0600

    Create main component

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import $ from 'jquery';
+
+
+class App extends Component {
+  render (){
+    return (
+
+      <div><h2>Coding challenge for Ignite</h2></div>
+
+    )
+  }
+}
+
+
+export default App;
